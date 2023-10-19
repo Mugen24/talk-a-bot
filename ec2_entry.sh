d87ce214afbf08bae42c7f878e565b0c8db20662
john.nguyen1022@gmail.com <john.nguyen1022@gmail.com>
2023-10-19 16:15:33 +1100

    fix secretes

--- ec2_entry.sh
+++ ec2_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-token=${aws ssm get-parameter --name talk-a-bot_token | jq .Parameter.Value}
-guildId=${aws ssm get-parameter --name talk-a-bot_guidId | jq .Parameter.Value}
-clientId=${aws ssm get-parameter --name talk-a-bot_clientId | jq .Parameter.Value}
+token=$(aws ssm get-parameter --name talk-a-bot_token | jq .Parameter.Value)
+guildId=$(aws ssm get-parameter --name talk-a-bot_guidId | jq .Parameter.Value)
+clientId=$(aws ssm get-parameter --name talk-a-bot_clientId | jq .Parameter.Value)
 
-echo $token
+node app.js
