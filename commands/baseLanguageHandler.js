43e888fb260f68737332c972b8809b965a4ad100
John Nguyen <John.nguyen10222gmail.com>
2023-10-24 22:32:46 +1100

    remove commands folder

--- commands/baseLanguageHandler.js
+++
@@ -1,5 +0,0 @@
-//customReplyFunc(originalText, translation)
-//Return a string for bot to reply
-const { translate } = require("../utils");
-const { BaseInteraction, Message } = require("discord.js");
-
